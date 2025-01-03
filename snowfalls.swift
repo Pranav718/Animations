559f5f266310e01432ed9feac9d95416d25fa675
Pranav Ray <148552902+Pranav718@users.noreply.github.com>
2025-01-03 21:58:26 +0530

    snowfalls.swift

---
+++ snowfalls.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  SomeAnimations
+//
+//  Created by Pranav Ray on 01/01/25.
+
+import SwiftUI
+
+struct Snowflake: Identifiable {
+    let id = UUID()
+    var position: CGPoint
+    var size: CGFloat
+}
+
+struct SnowfallView: View {
+    @State private var snowflakes: [Snowflake] = []
+    @State private var timer: Timer?
+    
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            ForEach(snowflakes) { snowflake in
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: snowflake.size, height: snowflake.size)
+                    .position(snowflake.position)
+                    .animation(.linear(duration: 5).repeatForever(autoreverses: false), value: snowflake.position)
+                    .onAppear {
+                        startFalling(snowflake: snowflake)
+                    }
+            }
+        }
+        .onAppear {
+            startSnowfall()
+        }
+    }
+    
+    private func startSnowfall() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+            let size = CGFloat.random(in: 5...15) // Random size for the snowflake
+            let xPosition = CGFloat.random(in: 0...UIScreen.main.bounds.width) // Random x position
+            let snowflake = Snowflake(position: CGPoint(x: xPosition, y: -size), size: size)
+            snowflakes.append(snowflake)
+        }
+    }
+    
+    func startFalling(snowflake: Snowflake) {
+        withAnimation {
+            if let index = snowflakes.firstIndex(where: { $0.id == snowflake.id }) {
+                snowflakes[index].position.y = UIScreen.main.bounds.height + snowflake.size
+            }
+        }
+    }
+    
+    func cleanup() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        SnowfallView()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
