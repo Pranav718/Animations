fbcdf1641a0dc212bbd4dd6e8b5c8a717153f884
Pranav Ray <148552902+Pranav718@users.noreply.github.com>
2025-01-04 22:53:10 +0530

    particlePlayground.swift

--- particlePlayground.swift
+++ particlePlayground.swift
@@ -34,3 +34,85 @@ struct Particle: Identifiable {
         scale *= 0.995
     }
 }
+
+struct ParticleSystem: View {
+    @State private var particles: [Particle] = []
+    @State private var timer: Timer?
+    let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            Canvas { context, size in
+                for particle in particles {
+                    let rect = CGRect(
+                        x: particle.position.x - particle.scale/2,
+                        y: particle.position.y - particle.scale/2,
+                        width: particle.scale,
+                        height: particle.scale
+                    )
+                    
+                    context.opacity = particle.opacity
+                    context.fill(
+                        Circle().path(in: rect),
+                        with: .color(particle.color)
+                    )
+                }
+            }
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { gesture in
+                        addParticles(at: gesture.location)
+                    }
+            )
+            .onChange(of: timeline.date) { oldValue, newValue in
+                updateParticles()
+            }
+        }
+        .onAppear {
+            startSystem()
+        }
+        .onDisappear {
+            timer?.invalidate()
+        }
+    }
+    
+    func startSystem() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { _ in
+            particles = particles.filter { 
+                 $0.opacity > 0.1
+            }
+        }
+    }
+    
+    func addParticles(at position: CGPoint) {
+        for _ in 0..<5 {
+            let particle = Particle(
+                position: position,
+                velocity: CGPoint(
+                    x: Double.random(in: -5...5),
+                    y: Double.random(in: -5...5)
+                ),
+                scale: CGFloat.random(in: 10...30),
+                opacity: 1.0,
+                color: colors.randomElement() ?? .white
+            )
+            particles.append(particle)
+        }
+    }
+    
+    func updateParticles() {
+        for i in particles.indices {
+            particles[i].update(in: UIScreen.main.bounds.size)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ParticleSystem()
+    }
+}
+
+#Preview {
+    ContentView()
+}
