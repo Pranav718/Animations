a9f34f7f75752109902d74e6431aa5c02ac664b4
Pranav Ray <148552902+Pranav718@users.noreply.github.com>
2025-01-06 22:52:50 +0530

    sparkle.swift

--- sparkle.swift
+++ sparkle.swift
@@ -1,1 +1,74 @@
+//
+//  ContentView.swift
+//  SomeAnimations
+//
+//  Created by Pranav Ray on 01/01/25.
+
+
+
+import SwiftUI
+
+struct SparkleView: View {
+    @State private var isAnimating = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.yellow)
+                .frame(width: 20, height: 20)
+                .scaleEffect(isAnimating ? 0.5 : 1.0)
+                .opacity(isAnimating ? 0.0 : 1.0)
+
+            ForEach(0..<8) { index in
+                RayView(angle: .degrees(Double(index) * 45))
+                    .offset(x: isAnimating ? 30 : 0)
+                    .rotationEffect(.degrees(Double(index) * 45))
+                    .opacity(isAnimating ? 0.0 : 1.0)
+            }
+        }
+        .onAppear {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 1.0)
+                    .repeatForever(autoreverses: true)
+            ) {
+                isAnimating = true
+            }
+        }
+    }
+}
+
+struct RayView: View {
+    let angle: Angle
+    
+    var body: some View {
+        Rectangle()
+            .fill(Color.yellow)
+            .frame(width: 10, height: 3)
+    }
+}
+
+struct SparkleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            SparkleView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            SparkleView()
+                .frame(width: 100, height: 100)
+        }
+    }
+}
+
+#Preview{
+    ContentView()
+}
+
 
